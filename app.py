483989225f9c9975c6ca06830d8deefac05fdd7e
hannasabrynna <hannasabrynna135@gmail.com>
2024-06-28 18:01:06 -0300

    Funcionalidades

--- app.py
+++ app.py
@@ -27,18 +27,51 @@ musics = [
         'id': 5,
         'name': 'Kill Bill',
         'singer': 'SZA'
-    },
+    }
 ]
 
-### Consultar Todas as Musicas
+##### Consultar Todas as Musicas #####
 
-### Consultar Apenas uma Musica (com o ID)
+@app.route('/musics', methods=['GET']) #methods esta informando que eus so aceito o metodo GET
+def get_musics():
+    return jsonify(musics)#Retorna os livros em formato Json
 
-### Adicionar nova Musica
+##### Consultar Apenas uma Musica (com o ID) #####
 
-### Editar Musica 
+@app.route('/musics/<int:id>', methods=['GET']) #<int:id> - Esta falando que eu espero um numero inteiro que sera nosso id
+def get_musics_by_id(id):
+    for music in musics: #o laço percorre o dicionario de livros
+        if music.get('id') == id: #verifica se o id passado é igual ao id do liivro que o laço esta percorrendo
+            return jsonify(music)
+        
+###### Adicionar nova Musica ######
+#
 
-### Deletar Musica
+@app.route('/musics', methods=['POST'])
+def add_new_music():
+    new_music = request.get_json() #request está pegando o novo valor e guardando em "new_music" 
+    musics.append(new_music) #Adicona a nova musica no dicionario de musicas
+    return jsonify(musics)
+
+###### Editar Musica ######
+
+@app.route('/musics/<int:id>', methods=['PUT'])
+def edit_music_by_id(id):
+    changes = request.get_json() 
+    for indice, music in enumerate(musics):
+        if music.get('id') == id: #Verifica o ID 
+            musics[indice].update(changes) #Faz a substituição do valor (UPDATE) para o valor da variavel "changes"
+            return jsonify(musics[indice]) #Retorna o valor alterado para o usuario
+        
+###### Deletar Musica ######
+
+@app.route('/musics/<int:id>', methods=['DELETE'])
+def delete_music_by_id(id):
+    for indice, music in enumerate(musics):
+        if music.get('id') == id: #Verifica o ID 
+            del musics[indice]
+    
+    return jsonify(musics) #Retorna 
 
 ## Defindo o localhost
 app.run(port=500, host='localhost', debug=True)
